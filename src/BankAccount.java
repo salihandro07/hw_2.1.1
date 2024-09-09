705d009a482e0ebc1b49f6207e38e4f3382dfee0
salihandro07 <khamrayumovsalih2gmail.com>
2024-09-09 11:08:33 +0600

    Initial commit

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class BankAccount {
+public class BankAccount {
+    private double amount;
+
+    public BankAccount() {
+        this.amount = 0;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void deposit(double sum) {
+        if (sum > 0) {
+            amount += sum;
+        }
+    }
+
+    public void withDraw(int sum) throws LimitException {
+        if (sum > amount) {
+            throw new LimitException("Недостаточно средств на счете для снятия", amount);
+        } else {
+            amount -= sum;
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BankAccount account = new BankAccount();
+
+        account.deposit(15000);
+
+        System.out.println("Текущий остаток: " + account.getAmount());
+
+        while (true) {
+            try {
+                System.out.println("Попытка снять 6000 сом...");
+                account.withDraw(6000);
+                System.out.println("Снято успешно. Остаток: " + account.getAmount());
+            } catch (LimitException e) {
+                System.out.println("Недостаточно средств для снятия 6000 сом. Остаток: " + e.getRemainingAmount());
+                System.out.println("Снятие оставшейся суммы: " + e.getRemainingAmount());
+                try {
+                    account.withDraw((int) e.getRemainingAmount());
+                } catch (LimitException ex) {
+                    System.out.println("Ошибка: " + ex.getMessage());
+                }
+                System.out.println("Остаток после снятия: " + account.getAmount());
+                break;
+            }
+        }
     }
 }
